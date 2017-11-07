4f2ff6492a1f15443e4e7eb05707ff1859858809
Colin Coleman <cc@csquared.me.uk>
2017-11-07 17:01:24 +0100

    trying to get vault variables into a dynamically generated settings.xml

---
+++ ci/tasks/make-settings-and-maven-deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+M2_HOME="${HOME}/.m2"
+M2_CACHE="${ROOT_FOLDER}/maven"
+
+echo "Generating symbolic links for caches"
+
+[[ -d "${M2_CACHE}" && ! -d "${M2_HOME}" ]] && ln -s "${M2_CACHE}" "${M2_HOME}"
+
+echo "Writing maven settings to [${M2_HOME}/settings.xml]"
+
+cat > "${M2_HOME}/settings.xml" <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<settings>
+	<servers>
+        <server>
+            <id>telia-iot-releases</id>
+            <username>$nexus-deploy-username</username>
+            <password>$nexus-deploy-password</password>
+        </server>
+        <server>
+            <id>telia-iot-snapshots</id>
+            <username>$nexus-deploy-username</username>
+            <password>$nexus-deploy-password</password>
+        </server>
+	</servers>
+</settings>
+EOF
+echo "Settings xml written"
+cat ${M2_HOME}/settings.xml
+mvn -f git-repo/pom.xml test
