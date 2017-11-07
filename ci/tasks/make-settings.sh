f90ea095d87c3a06861736772f2802e7d2c345cb
Colin Coleman <cc@csquared.me.uk>
2017-11-07 20:04:49 +0100

    removed simlinks and things from make-settings

--- ci/tasks/make-settings.sh
+++ ci/tasks/make-settings.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
-
-M2_HOME="${HOME}/.m2"
-M2_CACHE="${ROOT_FOLDER}/maven"
-
-echo "Generating symbolic links for caches"
-
-[[ -d "${M2_CACHE}" && ! -d "${M2_HOME}" ]] && ln -s "${M2_CACHE}" "${M2_HOME}"
-
-echo "Writing maven settings to [${M2_HOME}/settings.xml]"
-
-cat > "${M2_HOME}/settings.xml" <<EOF
+cat > "settings.xml" <<EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <settings>
 	<servers>
@@ -27,4 +17,4 @@ cat > "${M2_HOME}/settings.xml" <<EOF
 </settings>
 EOF
 echo "Settings xml written"
-cat ${M2_HOME}/settings.xml
+cat settings.xml
