4e690748a2e793813090053969d74cf90347ae4f
Colin Coleman <cc@csquared.me.uk>
2017-11-09 13:51:56 +0100

    fixed relative path bug

--- ci/tasks/docker-build.sh
+++ ci/tasks/docker-build.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 set -e
-cd /git-repo/docker
+cd ../../../docker
 cp -p "../target/$(ls -t ../target/*.jar | grep -v /orig | head -1)" app.jar
 
 docker build -t terraform-ci-poc .
-
-docker.withRegistry('https://752583717420.dkr.ecr.eu-west-1.amazonaws.com','ecr:eu-west-1:aws-deploy') {
-        docker.image('terraform-ci-poc').push('latest')
+docker push 752583717420.dkr.ecr.eu-west-1.amazonaws.com/terraform-ci-poc:latest
