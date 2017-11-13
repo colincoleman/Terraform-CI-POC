52c4d2417037f92187c730680604080d01ad03d3
Colin Coleman <cc@csquared.me.uk>
2017-11-13 16:03:50 +0100

    fixed path to created jar file

--- ci/tasks/maven-deploy/task.sh
+++ ci/tasks/maven-deploy/task.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mvn -f "git-repo/pom.xml" --settings "settings/settings.xml" deploy
-cp -p "git-repo/target/$(ls -t ../target/*.jar | grep -v /orig | head -1)" app.jar
+cp -p "git-repo/target/$(ls -t gitrepo/target/*.jar | grep -v /orig | head -1)" app.jar
