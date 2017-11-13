9047f4ed0bbcc357e3fa45978bb284c51e34052f
Colin Coleman <cc@csquared.me.uk>
2017-11-13 15:53:45 +0100

    docker build  - jar added as input

--- ci/tasks/docker-build/task.sh
+++ ci/tasks/docker-build/task.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 #set -e
-
+cp -p "jar-file/app.jar" git-repo/docker/app.jar
 echo `pwd`
 echo "hello"
-#cp -p "../target/$(ls -t ../target/*.jar | grep -v /orig | head -1)" app.jar
+cd git-repo/docker
+echo `pwd`
+
 
 #docker build -t terraform-ci-poc .
 #docker push 752583717420.dkr.ecr.eu-west-1.amazonaws.com/terraform-ci-poc:latest
--- ci/tasks/maven-deploy/task.sh
+++ ci/tasks/maven-deploy/task.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 mvn -f "git-repo/pom.xml" --settings "settings/settings.xml" deploy
+cp -p "git-repo/target/$(ls -t ../target/*.jar | grep -v /orig | head -1)" app.jar
