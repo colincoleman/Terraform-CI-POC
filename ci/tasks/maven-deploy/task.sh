608043f6fda53f4cbb55b6461aff55d964a47b68
Colin Coleman <cc@csquared.me.uk>
2017-11-13 16:20:22 +0100

    fix path error

--- ci/tasks/maven-deploy/task.sh
+++ ci/tasks/maven-deploy/task.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mvn -f "git-repo/pom.xml" --settings "settings/settings.xml" deploy
-cp -p "git-repo/target/$(ls -t git-repo/target/*.jar | grep -v /orig | head -1)" jar-file/app.jar
+cp -p "$(ls -t git-repo/target/*.jar | grep -v /orig | head -1)" jar-file/app.jar
