9823cf20351f22497a52b0676f61ab5d8db33c55
Colin Coleman <cc@csquared.me.uk>
2017-11-14 13:56:42 +0100

    updated docker image used for dockerbuild to one that includes docker-in-docker and aws cli

--- ci/tasks/docker-build/task.sh
+++ ci/tasks/docker-build/task.sh
@@ -5,7 +5,5 @@ echo `pwd`
 echo "hello"
 cd git-repo/docker
 echo `pwd`
-
-
-#docker build -t terraform-ci-poc .
+docker build -t terraform-ci-poc .
 #docker push 752583717420.dkr.ecr.eu-west-1.amazonaws.com/terraform-ci-poc:latest
