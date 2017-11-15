318063ab8b112f1fe6d4b56a00a398164ed96b2c
Colin Coleman <cc@csquared.me.uk>
2017-11-15 11:10:40 +0100

    check if file copied

--- ci/tasks/docker-build/task.sh
+++ ci/tasks/docker-build/task.sh
@@ -5,5 +5,6 @@ echo `pwd`
 echo "hello"
 cd git-repo/docker
 echo `pwd`
+echo `ls -al`
 #docker build -t terraform-ci-poc .
 #docker push 752583717420.dkr.ecr.eu-west-1.amazonaws.com/terraform-ci-poc:latest
