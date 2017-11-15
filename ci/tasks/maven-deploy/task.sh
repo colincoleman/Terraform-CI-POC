7a954f1a9763c5ebad6868cdd6a2c517ab8ee65e
Colin Coleman <cc@csquared.me.uk>
2017-11-15 10:47:06 +0100

    copied jar file to right place for docker build

--- ci/tasks/maven-deploy/task.sh
+++ ci/tasks/maven-deploy/task.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 mvn -f "git-repo/pom.xml" --settings "settings/settings.xml" deploy
 cp -p "$(ls -t git-repo/target/*.jar | grep -v /orig | head -1)" jar-file/app.jar
+cp jar-file/app.jar git-repo/docker/toRoot/app.jar
