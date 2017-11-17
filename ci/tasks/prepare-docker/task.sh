7747a0ee748f4b0e58fa04bcc0aa5dbccd8d51ad
Colin Coleman <cc@csquared.me.uk>
2017-11-17 15:37:44 +0100

    removed quotes

--- ci/tasks/prepare-docker/task.sh
+++ ci/tasks/prepare-docker/task.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 cp -p -r git-repo/docker/ docker-folder
-echo "resources copies to docker workspace "
-cp -p "jar-file/app.jar" docker-folder/toRoot/app.jar
+echo "resources copied to docker workspace "
+cp -p jar-file/app.jar docker-folder/toRoot/app.jar
 echo "app.jar copied to docker workspace "
