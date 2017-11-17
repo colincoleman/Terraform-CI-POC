fdbe93488b29f5aa94928000b6eb0bb7302277d3
Colin Coleman <cc@csquared.me.uk>
2017-11-17 15:06:26 +0100

    changed how docker folder created

--- ci/tasks/prepare-docker/task.sh
+++ ci/tasks/prepare-docker/task.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 set -e
-cp -p "jar-file/app.jar" git-repo/docker/toRoot/app.jar
+cp -p -r git-repo/docker/ docker-folder
+echo "resources copies to docker workspace "
+cp -p "jar-file/app.jar" docker-folder/toRoot/app.jar
 echo "app.jar copied to docker workspace "
