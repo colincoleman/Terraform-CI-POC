5bb7412855fb5e92eecce52256fad2f8d11b78bf
Colin Coleman <cc@csquared.me.uk>
2017-11-20 09:38:09 +0100

    another test

--- ci/tasks/prepare-docker/task.sh
+++ ci/tasks/prepare-docker/task.sh
@@ -3,5 +3,6 @@ set -e
 cp -p -r git-repo/docker/ docker-folder
 echo "resources copied to docker workspace "
 echo `ls`
+echo `ls jar-file/`
 cp jar-file/app.jar docker-folder/toRoot/app.jar
 echo "app.jar copied to docker workspace "
