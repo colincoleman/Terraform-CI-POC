123915bbb4fc5b893454b7fec5b8d787c6f6533d
Colin Coleman <cc@csquared.me.uk>
2017-11-22 12:46:50 +0100

    fixed path error

--- ci/tasks/prepare-docker/task.sh
+++ ci/tasks/prepare-docker/task.sh
@@ -4,5 +4,5 @@ cp -p -r git-repo/docker/ docker-folder
 echo "resources copied to docker workspace "
 echo `ls`
 echo `ls docker-folder/`
-cp -p jar-file/app.jar docker-folder/toRoot/app.jar
+cp -p jar-file/app.jar docker-folder/docker/toRoot/app.jar
 echo "app.jar copied to docker workspace "
