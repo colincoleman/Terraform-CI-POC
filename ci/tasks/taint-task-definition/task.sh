907e40c498825a57b7f009c6c7cad2f3579b5eee
Colin Coleman <colin.coleman@kontrollsmart.com>
2017-12-08 12:47:37 +0100

    test

--- ci/tasks/taint-task-definition/task.sh
+++ ci/tasks/taint-task-definition/task.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 cd git-repo/terraform/env-dev
-echo `pwd`
-echo `ls`
+echo $AWS_ACCESS_KEY_ID
 terraform init
-terraform taint aws_ecs_task_definition.terraform-ci-poc
+#terraform taint aws_ecs_task_definition.terraform-ci-poc
