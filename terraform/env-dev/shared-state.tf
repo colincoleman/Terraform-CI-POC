e1693d97952a274bee15bb636c7e8d4dbb1a403b
Colin Coleman <colin.coleman@kontrollsmart.com>
2017-12-08 10:24:11 +0100

    changed shared state to be on tn-stage

--- terraform/env-dev/shared-state.tf
+++ terraform/env-dev/shared-state.tf
@@ -2,15 +2,15 @@
 # State/provider
 # ------------------------------------------------------------------------------
 terraform {
-  required_version = "0.10.7"
+  required_version = "0.11.1"
 
   backend "s3" {
     key            = "terraform-ci-poc/terraform-ci-poc.tfstate"
-    bucket         = "752583717420-terraform-state"
-    dynamodb_table = "752583717420-terraform-state"
+    bucket         = "276208424594-terraform-state"
+    dynamodb_table = "276208424594-terraform-state"
     acl            = "bucket-owner-full-control"
     encrypt        = "true"
-    kms_key_id     = "arn:aws:kms:eu-west-1:752583717420:key/86801c43-5eb3-4729-af1c-7fe9ff1ba54d"
+    kms_key_id     = "arn:aws:kms:eu-west-1:276208424594:key/90b6fd6f-925c-4426-88e6-214955de0030"
     region         = "eu-west-1"
   }
 }
@@ -18,5 +18,5 @@ terraform {
 provider "aws" {
   version             = "1.1.0"
   region              = "${var.region}"
-  allowed_account_ids = ["752583717420"]
+  allowed_account_ids = ["276208424594"]
 }
