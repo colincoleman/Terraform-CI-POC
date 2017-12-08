9c77377fffa8ad4b3aa7cddf0fddb29d8eb2bcaa
Colin Coleman <colin.coleman@kontrollsmart.com>
2017-12-08 14:47:26 +0100

    added plan

--- ci/tasks/taint-task-definition/task.sh
+++ ci/tasks/taint-task-definition/task.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cd git-repo/terraform/env-dev
 terraform init
-terraform taint aws_ecs_task_definition.terraform-ci-poc
+/terraform taint aws_ecs_task_definition.terraform-ci-poc
+terraform plan
