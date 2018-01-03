a300bd0bf6bdbcce308fcf629f435290f4af5612
Colin Coleman <colin.coleman@kontrollsmart.com>
2018-01-03 14:58:43 +0100

    another echo test

--- ci/tasks/maven-deploy/task.sh
+++ ci/tasks/maven-deploy/task.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+echo `ls -al`
 M2_HOME=${HOME}/.m2
 mkdir -p ${M2_HOME}
 M2_LOCAL_REPO="${ROOT_FOLDER}/.m2"
 mkdir -p "${M2_LOCAL_REPO}/repository"
-echo `ls -al`
+echo `ls -al .m2/`
 
 #mvn -f "git-repo/pom.xml" --settings "settings/settings.xml" deploy
 #cp -p "$(ls -t git-repo/target/*.jar | grep -v /orig | head -1)" jar-file/app.jar
