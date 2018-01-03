680c0a1919a85d54056a8e08a9f8ecaf4bdaa021
Colin Coleman <colin.coleman@kontrollsmart.com>
2018-01-03 15:46:09 +0100

    clean up removal of echo lines

--- ci/tasks/maven-deploy/task.sh
+++ ci/tasks/maven-deploy/task.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 M2_LOCAL_REPO=".m2"
 mkdir -p "${M2_LOCAL_REPO}/repository"
-echo `ls -al .m2/`
 mvn -f "git-repo/pom.xml" --settings "settings/settings.xml" deploy
-echo `ls -al .m2/repository/`
-
 cp -p "$(ls -t git-repo/target/*.jar | grep -v /orig | head -1)" jar-file/app.jar
