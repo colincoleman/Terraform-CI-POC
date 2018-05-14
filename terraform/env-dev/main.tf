a966544c565bcd2249d3b0cf15469c269eb98190
Colin Coleman <colin.coleman@kontrollsmart.com>
2018-05-14 16:29:47 +0200

    Update to test PR of spotfleet-cluster

--- terraform/env-dev/main.tf
+++ terraform/env-dev/main.tf
@@ -1,5 +1,11 @@
+provider "aws" {
+  version             = "1.17.0"
+  region              = "eu-west-1"
+  allowed_account_ids = ["653090749433"]
+}
+
 module "this-poc" {
   source               = "../modules/terraform-ci-poc"
   repository_uri       = "653090749433.dkr.ecr.eu-west-1.amazonaws.com/terraform-ci-poc"
-  private_subnet_count = "3"
+  private_subnet_count = 3
 }
--- terraform/modules/terraform-ci-poc/main.tf
+++ terraform/modules/terraform-ci-poc/main.tf
@@ -1,7 +1,7 @@
 data "aws_caller_identity" "current" {}
 
 module "vpc" {
-  source          = "github.com/TeliaSoneraNorge/telia-terraform-modules//ec2/vpc?ref=4d643bb"
+  source          = "github.com/TeliaSoneraNorge/telia-terraform-modules//ec2/vpc?ref=b636148"
   prefix          = "${var.prefix}"
   cidr_block      = "${var.vpc-cidr_block}"
   private_subnets = "${var.private_subnet_count}"
@@ -10,7 +10,7 @@ module "vpc" {
 }
 
 module "lb" {
-  source     = "github.com/TeliaSoneraNorge/telia-terraform-modules//ec2/lb?ref=4d643bb"
+  source     = "github.com/TeliaSoneraNorge/telia-terraform-modules//ec2/lb?ref=b636148"
   prefix     = "${var.prefix}"
   type       = "application"
   internal   = "false"
@@ -19,48 +19,59 @@ module "lb" {
   tags       = "${var.tags}"
 }
 
+resource "aws_lb_listener" "main" {
+  load_balancer_arn = "${module.lb.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = "${module.terraform-ci-poc-service.target_group_arn}"
+    type             = "forward"
+  }
+}
+
 resource "aws_security_group_rule" "ingress_80" {
   security_group_id = "${module.lb.security_group_id}"
   type              = "ingress"
   protocol          = "tcp"
   from_port         = "80"
   to_port           = "80"
-
-  cidr_blocks = [
-    "0.0.0.0/0",
-  ]
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 module "cluster" {
-  source              = "github.com/TeliaSoneraNorge/telia-terraform-modules//ecs/spotfleet-cluster?ref=4d643bb"
+  source              = "github.com/TeliaSoneraNorge/telia-terraform-modules//ecs/spotfleet-cluster?ref=b636148"
   prefix              = "${var.prefix}"
-  subnets             = ["${module.vpc.private_subnet_ids}"]
-  subnet_count        = "${var.private_subnet_count}"
-  target_capacity     = "3"
+  subnets             = "${module.vpc.private_subnet_ids}"
+  subnet_count        = "3"
+  target_capacity     = 3
   allocation_strategy = "lowestPrice"
   tags                = "${var.tags}"
   spot_price          = "0.02"
   load_balancers      = ["${module.lb.security_group_id}"]
   load_balancer_count = "1"
+  vpc_id              = "${module.vpc.vpc_id}"
 }
 
 module "terraform-ci-poc-service" {
-  source          = "github.com/TeliaSoneraNorge/divx-terraform-modules//ecs/service?ref=4d643bb"
+  source          = "github.com/TeliaSoneraNorge/divx-terraform-modules//ecs/service?ref=b636148"
   prefix          = "${var.prefix}"
   vpc_id          = "${module.vpc.vpc_id}"
   cluster_id      = "${module.cluster.id}"
   cluster_role_id = "${module.cluster.role_id}"
+
   target {
-    protocol = "HTTP"
-    port = "5050"
-    load_balancer="${module.lb.arn}"
+    protocol      = "HTTP"
+    port          = "5050"
+    load_balancer = "${module.lb.arn}"
   }
-  health{
-    port = "traffic-port"
-    path = "/"
+
+  health {
+    port    = "traffic-port"
+    path    = "/"
     matcher = "200"
   }
-  task_definition_image = "${var.repository_uri}:latest"
-  task_container_count = "2"
 
+  task_definition_image = "${var.repository_uri}:latest"
+  task_container_count  = "2"
 }
